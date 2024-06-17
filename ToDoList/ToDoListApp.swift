9175e727fd6b6f0b02288babde2406802af52197
ijkVector <johnsmith28122000@gmail.com>
2024-06-17 13:06:57 +0300

    Add: .gitignore

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Иван Дроботов on 17.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
