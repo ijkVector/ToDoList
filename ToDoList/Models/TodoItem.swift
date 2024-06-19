0420300a09ce6e28349e02dd0bceb0b084ee402a
ijkVector <johnsmith28122000@gmail.com>
2024-06-20 02:36:51 +0300

    Add: parse(json: Any)

--- ToDoList/Models/TodoItem.swift
+++ ToDoList/Models/TodoItem.swift
@@ -13,6 +13,7 @@ enum Importance: String, Codable {
     case important
 }
 
+//MARK: - TodoItem Defenition
 struct TodoItem: Codable {
     
     let id: String
@@ -47,3 +48,46 @@ struct TodoItem: Codable {
         self.modifiedDate = modifiedDate
     }
 }
+
+//MARK: - Parse JSON
+extension TodoItem {
+    //MARK: - From Json to TodoItem
+    static func parse(json: Any) -> TodoItem? {
+        guard let jsonAsString = json as? String else { return nil }
+        let jsonData = Data(jsonAsString.utf8)
+        
+        guard let jsonAsDictionary = try? JSONSerialization.jsonObject(with: jsonData) as? [String: Any],
+              let id = jsonAsDictionary["id"] as? String,
+              let text = jsonAsDictionary["text"] as? String,
+              let isFinished = jsonAsDictionary["isFinished"] as? Bool,
+              let сreationTimeInterval = jsonAsDictionary["сreationDate"] as? TimeInterval else { return nil }
+        
+        let сreationDate = Date(timeIntervalSince1970: сreationTimeInterval)
+        var importance: Importance = .routine
+        var deadline: Date?
+        var modifiedDate: Date?
+        
+        if let rawValue = jsonAsDictionary["importance"] as? String, let value = Importance(rawValue: rawValue) {
+            importance = value
+        }
+        
+        if let deadlineTimeInterval = jsonAsDictionary["deadline"] as? TimeInterval {
+            deadline = Date(timeIntervalSince1970: deadlineTimeInterval)
+        }
+        
+        if let modifiedDateTimeInterval = jsonAsDictionary["modifiedDate"] as? TimeInterval {
+            modifiedDate = Date(timeIntervalSince1970: modifiedDateTimeInterval)
+        }
+        
+        return TodoItem(
+            id: id,
+            text: text,
+            importance: importance,
+            deadline: deadline,
+            isFinished: isFinished,
+            сreationDate: сreationDate,
+            modifiedDate: modifiedDate
+        )
+    }
+}
+
